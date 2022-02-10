e52630716a3ee8758f87f1846841e197c9c2ae21
telanet <benardngetich@gmail.com>
2022-02-10 06:25:11 +0300

    Add content to index.html and style the header with style.css

---
+++ script.js
@@ -0,0 +1,32 @@
+const singers = [
+    {
+    name: 'Benard',
+    genre: 'Reggae',
+    details: 'lorem ipsum dolor sit amet, consectetur adip'
+    picture: '',
+},
+{
+    name: 'Benard',
+    genre: 'Reggae',
+    details: 'lorem ipsum dolor sit amet, consectetur adip'
+    picture: '',
+}
+{
+    name: 'Benard',
+    genre: 'Reggae',
+    details: 'lorem ipsum dolor sit amet, consectetur adip'
+    picture: '',
+}
+{
+    name: 'Benard',
+    genre: 'Reggae',
+    details: 'lorem ipsum dolor sit amet, consectetur adip'
+    picture: '',
+}
+{
+    name: 'Benard',
+    genre: 'Reggae',
+    details: 'lorem ipsum dolor sit amet, consectetur adip'
+    picture: '',
+}
+]
